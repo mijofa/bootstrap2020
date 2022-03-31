12c177ca1add6becd98f2daecb6dcacaa01a99e9
Mike Abrahall <mike@abrahall.id.au>
2022-03-31 20:45:31 +1100

    Better handle multiple SRV records. Improved logging

--- jellyfin-media-player/usr-local-bin-set-jellyfin-server.py
+++ jellyfin-media-player/usr-local-bin-set-jellyfin-server.py
@@ -20,12 +20,8 @@ CREDENTIALS_KEY = b'_file://\x00\x01jellyfin_credentials'
 AUTOLOGIN_KEY = b'_file://\x00\x01enableAutoLogin'
 
 
-def guess_base_url():
-    """
-    Query DNS for probably Jellyfin server.
-
-    FIXME: Does Jellyfin already use avahi? Can we use that instead of SRV records?
-    """
+def get_sorted_SRV(SRV):
+    """Get the top SRV record for the given query."""
     # Python was doing some really weird & stupid things when trying to get the search domain.
     # So I gave up and called out to resolvectl instead
     # FIXME: Why the fuck didn't python's socket module work?!?
@@ -40,23 +36,50 @@ def guess_base_url():
         if domain:
             break
 
-    # Get all SRV records and sort them by weight,
+    # Get all SRV records and sort them by priority,
     # then grab only the first one
     # FIXME: Try them in order until 1 works?
     # FIXME: Is this sorted in the right order?
-    srv_records = list(dns.resolver.resolve(f'_jellyfin._tcp.{domain}', 'SRV'))
-    srv_records.sort(key=lambda i: i.weight)
+    srv_records = list(dns.resolver.resolve(f'{SRV}.{domain}', 'SRV'))
+    srv_records.sort(key=lambda i: i.priority)
 
-    jf_port = srv_records[0].port
-    jf_target = str(srv_records[0].target).rstrip('.')
-    # If the port is a usual https port, then do https, otherwise http
-    # NOTE: Jellyfin defaults to 8920 for https
-    if jf_port in (443, 8920):
-        jf_proto = 'https'
-    else:
-        jf_proto = 'http'
+    return srv_records
 
-    return f"{jf_proto}://{jf_target}:{jf_port}"
+
+def get_JF_info(base_url):
+    """Query the info directly from the Jellyfin server (similar to how the Chromecast does)."""
+    # FIXME: If the LocalAddress is missing we're supposed to use the ManualAddress,
+    #        but that could result in finding the server this is running on, which would be a problem.
+    with urllib.request.urlopen(urllib.parse.urljoin(base_url, "System/Info/Public")) as server_query:
+        data = json.load(server_query)
+        data["ManualAddress"] = base_url
+        return data
+
+
+def guess_JF_base_url():
+    """
+    Query DNS for Jellyfin server.
+
+    FIXME: Does Jellyfin already use avahi? Can we use that instead of SRV records?
+    """
+    SRV_records = get_sorted_SRV('_jellyfin._tcp')
+
+    for record in SRV_records:
+        jf_port = record.port
+        jf_target = str(record.target).rstrip('.')
+        # If the port is a usual https port, then do https, otherwise http
+        # NOTE: Jellyfin defaults to 8920 for https
+        if jf_port in (443, 8920):
+            jf_proto = 'https'
+        else:
+            jf_proto = 'http'
+
+        # If the server is uncontactable, or causes any issues getting the info we need,
+        # just move onto the next one.
+        try:
+            return get_JF_info(f"{jf_proto}://{jf_target}:{jf_port}")
+        except:  # noqa: E722
+            continue
 
 
 parser = argparse.ArgumentParser(description=__doc__)
@@ -64,28 +87,30 @@ parser.add_argument('base_url', default=None, type=str, nargs='?',
                     help="The base URL for the Jellyfin server (default: determined from SRV records)")
 # FIXME: The autologin part doesn't actually work currently
 parser.add_argument('--UserId', default=None, type=str,
-                    help="The UserId for autologin (default: no autologin) (requires --AccessToken and --deviceId2)")
+                    help="The UserId for autologin (default: no autologin) (requires --AccessToken)")
 parser.add_argument('--AccessToken', default=None, type=str,
-                    help="The AccessToken for autologin (default: no autologin) (requires --UserId and --deviceId2)")
+                    help="The AccessToken for autologin (default: no autologin) (requires --UserId)")
 args = parser.parse_args()
 
-base_url = args.base_url or guess_base_url()
-
-# Query the info from the Jellyfin server (similar to how the Chromecast does)
-with urllib.request.urlopen(urllib.parse.urljoin(base_url, "System/Info/Public")) as server_query:
-    server_info = json.load(server_query)
+if args.base_url:
+    server_info = get_JF_info(args.base_url)
+else:
+    server_info = guess_JF_base_url()
 
 # Massage that data into what's needed for the localstorage key
+# FIXME: What happens if multiple servers are set here?
+# FIXME: I should probably put every SRV response in here... but then how do we deal with the cache?
 jellyfin_credentials = {
     "Servers": [
         {
-            "ManualAddress": base_url,
+            "ManualAddress": server_info['ManualAddress'],
             "Id": server_info['Id'],
             # NOTE: Only ManualAddress & ID are *required*, but I've got the rest, so might as well.
             "Name": server_info['ServerName'],
             # When running behind a caching server, the LocalAddress will bypass the cache.
-            # My solution is to make the caching server remove the LocalAddress, in which case this code should not crash.
-            "LocalAddress": server_info['LocalAddress'] if "LocalAddress" in server_info else base_url,
+            # My solution is to make the caching server remove the LocalAddress,
+            # which used to crash this code so I threw the if/else in without testing if I could just leave the field blank.
+            "LocalAddress": server_info['LocalAddress'] if "LocalAddress" in server_info else server_info['ManualAddress'],
             # Optional args for auto login if configured
             # FIXME: Doesn't actually work yet
             "UserId": args.UserId,
@@ -93,6 +118,14 @@ jellyfin_credentials = {
         },
     ],
 }
+
+# Since that might have secrets in it, let's copy it out and remove them before we log it.
+sanitised_server_info = jellyfin_credentials['Servers'][0].copy()
+sanitised_server_info['UserId'] = ('[REDACTED]' if jellyfin_credentials['Servers'][0].get('UserId')
+                                   else jellyfin_credentials['Servers'][0].get('UserId'))
+sanitised_server_info['AccessToken'] = ('[REDACTED]' if jellyfin_credentials['Servers'][0].get('AccessToken')
+                                        else jellyfin_credentials['Servers'][0].get('AccessToken'))
+print('Jellyfin Credentials', sanitised_server_info)
 
 # Find or create the local storage database
 local_storage_path = pathlib.Path("~/.local/share/Jellyfin Media Player/QtWebEngine/Default/Local Storage").expanduser()
@@ -112,6 +145,8 @@ with contextlib.closing(plyvel.DB(os.fspath(local_storage_path / 'leveldb'), cre
     # by leaving autologin disabled then I can easily "log out" by simply restarting the application.
     if args.UserId and args.AccessToken:
         # But on some systems I may want to autologin when the credentials are supplied, so allow that too
+        print("Enabling autologin")
         leveldb.put(AUTOLOGIN_KEY, b'\x01true')
     else:
+        print("Disabling autologin")
         leveldb.put(AUTOLOGIN_KEY, b'\x01false')
