d260561f0ba5d6c4d1e209ce4cf3047e49cd91fc
Mike Abrahall <mike@abrahall.id.au>
2022-06-25 14:24:04 +1000

    Rework github_updater.py to actually work on fat32 without hardlinks/etc

--- jellyfin-media-player/local-boot-updater/github_updater.py
+++ jellyfin-media-player/local-boot-updater/github_updater.py
@@ -4,12 +4,35 @@ import sys
 import urllib.request
 import pathlib
 import hashlib
+import shutil
+import subprocess
 
 import github
 
 
 BOOT_PATH = pathlib.Path('.').resolve()
 GITHUB_REPO = sys.argv[1]
+
+
+def get_currently_booted_soe():
+    """Get the currently booted SOE version."""
+    # Sanity-check that the loopback device is mounted where it should be.
+    loop_mounts = [line.split() for line in pathlib.Path('/proc/mounts').read_text().splitlines()
+                   if line.startswith('/dev/loop0 ')]
+    assert all(mp in ('/run/live/rootfs/filesystem.squashfs', '/lib/live/mount/rootfs/filesystem.squashfs') and t == 'squashfs'
+               for d, mp, t, o, _, _, in loop_mounts), "Unexpected loopback mount(s)"
+
+    # Check what squashfs file the loopback device is actually attached to.
+    loop = subprocess.check_output(['losetup', '/dev/loop0'], text=True).strip().split()
+    if loop[-1] == '(deleted))':
+        # Well it's already been deleted somehow, just accept that and carry-on
+        return None
+
+    assert len(loop) == 3 and loop[0] == '/dev/loop0:', "Unexpected losetup output"
+    current_squashfs = pathlib.Path(loop[2].strip('()'))
+    assert current_squashfs.exists(), "Current squashfs doesn't exist"
+
+    return current_squashfs.parent.name
 
 
 def get_repo_latest_assets(repo_name):
@@ -25,11 +48,7 @@ def get_repo_latest_assets(repo_name):
     # FIXME: Should we check signatures/etc here?
     hash_filename, = [name for name in assets if name.endswith('SUMS')]
     hash_file = urllib.request.urlopen(assets[hash_filename].browser_download_url)
-    hash_data = {name.strip(): hash_sum for hash_sum, name in (
-        line.decode(hash_file.headers.get_content_charset() or 'utf-8').split(maxsplit=1) for line in hash_file.readlines())}
     hash_algo = hash_filename[:-4].lower()  # Just remove the 'SUMS' from the end
-
-    print(hash_algo, hash_data)
 
     if hash_algo == 'b2':
         # blake2b is the default for b2sum command, and is more efficient on 64-bit while blake2s is more efficient on 8/16/32-bit
@@ -42,88 +61,93 @@ def get_repo_latest_assets(repo_name):
     else:
         hash_func = getattr(hashlib, hash_algo)
 
-    for filename in hash_data:
+    hash_data = {name.strip(): hash_sum for hash_sum, name in (
+        line.decode(hash_file.headers.get_content_charset() or 'utf-8').split(maxsplit=1) for line in hash_file.readlines())}
+
+    for filename in assets:
         asset = assets[filename]
-        if hash_algo == 'sha3':
-            # Not sure if we should be allowing multiple different hash lengths in one file, but we do
-            assets[filename].hash_func = getattr(hashlib, 'sha3_' + str(len(hash_data[filename]) * 4))
-        else:
-            assets[filename].hash_func = hash_func
-        assets[filename].hash = hash_data[filename]
+        if filename in hash_data:
+            if hash_algo == 'sha3':
+                # Not sure if we should be allowing multiple different hash lengths in one file, but we do
+                assets[filename].hash_func = getattr(hashlib, 'sha3_' + str(len(hash_data[filename]) * 4))
+            else:
+                assets[filename].hash_func = hash_func
+            assets[filename].hash = hash_data[filename]
         yield asset
 
 
-def maybe_update_assets(repo_name: str, dest_dir: pathlib.Path):
+def maybe_get_new_assets(repo_name: str, old_dir: pathlib.Path, new_dir: pathlib.Path):
     """
-    Check if the assets in dest_dir need an update and updates them accordingly.
+    Check if the assets in old_dir need an update and updates them into new_dir accordingly.
 
-    Returns the number of assets that were updated
+    Returns the list of filenames that were updated
     """
-    num_assets_updated = 0
+    if not new_dir.exists():
+        new_dir.mkdir()
+
+    assets_updated = []
     for asset in get_repo_latest_assets(repo_name):
         if not hasattr(asset, 'hash'):
             # We can't confirm the hash of this file, so we don't even bother with it.
-            # It's probably he SUMS file itself.
+            # It might be the SUMS file itself.
+            # FIXME: I actually want to keep these, but only if we other assets got updated too
             # FIXME: What if it is a detached signature?
             #        and not (asset.name.endswith('.asc') or asset.name.endswith('.sig'))
+            print('Skipping', asset.name)
             continue
 
-        asset_path = dest_dir / asset.name
-        if asset_path.exists():
-            checked_hash = asset.hash_func(asset_path.read_bytes()).hexdigest()
-            if checked_hash != asset.hash:
-                print(asset.name, "doesn't match, unlinking.")
-                asset_path.unlink()
-            else:
-                print(asset.name, "all good!")
+        old_asset = old_dir / asset.name
+        new_asset = new_dir / asset.name
+        if new_asset.exists() and asset.hash_func(new_asset.read_bytes()).hexdigest() == asset.hash:
+            # Exists in pending, probably crashed mid-update
+            print(asset.name, "already pending, and hashes match. Skipping, but still adding to queue")
+            assets_updated.append(asset.name)
+        elif old_asset.exists() and asset.hash_func(old_asset.read_bytes()).hexdigest() == asset.hash:
+            # Exists in latest, as expected
+            print(asset.name, "exists in latest, and hashes match. Skipping")
+        else:
+            # Hash mismatch or doesn't exist
+            print(asset.name, "hash mismatch or doesn't exist. Downloading new")
 
-        if not asset_path.exists():
-            print(asset.name, "downloading...")
-            # FIXME: Use the reporthook= arg and do some sort of progress bar
-            urllib.request.urlretrieve(asset.browser_download_url, dest_dir / asset.name)
+            # FIXME: Can we do anything like an rsync update here?
+            # FIXME: Use the reporthook= arg and do some sort of progress bar, only if stdout is a TTY
+            urllib.request.urlretrieve(asset.browser_download_url, new_asset)
+            assets_updated.append(asset.name)
 
-            num_assets_updated += 1
-
-    return num_assets_updated
+    return assets_updated
 
 
-def increment_stored_releases(boot_dir: pathlib.Path):
+def increment_stored_releases(previous: pathlib.Path, latest: pathlib.Path, pending: pathlib.Path):
     """
-    Move latest to previous, and link pending into latest.
+    Increment the SOE versions so that we always have only previous & latest.
 
-    This is trying to do a kind of copy-on-write type thing,
-    for when using simpler FAT filesystems that don't support it at the fs level.
+    This is done by copying all of latest/* to pending/ without overwriting,
+    then moving latest to previous (if we're not currently running previous)
+    and moving pending to latest.
+
+    This would be better/easier if we were using a filesystem capable of copy-on-write,
+    or even just hardlinks. However I want this on the ESP directly, so we can't do that.
     """
-    previous = (boot_dir / 'previous')
-    latest = (boot_dir / 'latest')
-    pending = (boot_dir / 'pending')
     assert previous.is_dir() and latest.is_dir() and pending.is_dir()
 
-    # Delete all of 'previous'
-    # FIXME: 'previous' should actually be more like "last successful". Currently it's always 1 version behind latest
-    for previous_asset in previous.iterdir():
-        previous_asset.unlink()
+    # Copy latest to pending
+    shutil.copytree(latest, pending, dirs_exist_ok=True,
+                    # Ignore files already in pending
+                    ignore=lambda src, names: (path.name for path in pending.iterdir()))
 
-    # Link all of 'latest' into 'previous'
-    for latest_asset in latest.iterdir():
-        latest_asset.link_to(previous / latest_asset.name)
+    if get_currently_booted_soe() == previous.name:
+        # Don't update previous if we're still running that since the last update
+        print("Ignoring previous as it is what we are currently running")
+        shutil.rmtree(latest)
+    else:
+        # Otherwise, move latest to previous just in case the new version doesn't boot
+        print("Moving latest to previous")
+        shutil.rmtree(previous)
+        latest.rename(previous)
 
-    # Move all of 'pending' into 'latest' and immediately hardlink it back into 'pending'
-    # This is mostly only becuse link_to won't overwrite the target,
-    # so if I delete the target then do link_to there will be a small window where the file doesn't exist and the system won't boot
-    for pending_asset in pending.iterdir():
-        latest_asset = latest / pending_asset.name
-        if not (latest_asset.exists() and pending_asset.samefile(latest_asset)):
-            # Surprisingly replace() seems to be a no-op if the files are already the same hardlink anyway,
-            # which in-turn causes link_to to fail, so just don't do that
-            pending_asset.replace(latest_asset)
-            latest_asset.link_to(pending_asset)
-
-    # Delete any assets leftover in 'latest' which are not in 'pending'
-    for latest_asset in latest.iterdir():
-        if (pending / latest_asset.name) not in pending.iterdir():
-            latest_asset.unlink()
+    print("Moving pending to latest")
+    pending.rename(latest)
 
 
-if maybe_update_assets(GITHUB_REPO, BOOT_PATH / 'pending'):
-    increment_stored_releases(BOOT_PATH)
+if maybe_get_new_assets(GITHUB_REPO, BOOT_PATH / 'latest', BOOT_PATH / 'pending'):
+    increment_stored_releases(BOOT_PATH / 'previous', BOOT_PATH / 'latest', BOOT_PATH / 'pending')
