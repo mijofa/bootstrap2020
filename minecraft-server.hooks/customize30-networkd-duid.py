174ee5cd3b78757608233252a2a2f3448bb3b8ec
Mike Abrahall <mike@abrahall.id.au>
2023-02-22 23:29:04 +1100

    Hack around static DHCP not working

---
+++ minecraft-server.hooks/customize30-networkd-duid.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Configure systemd-networkd DHCP DUID.
+
+The default DUIDType is 'vendor', but my hardware doesn't have a vendor supplied UUID.
+In a non-live system this would be fine because it would fallback on /etc/machine-id,
+but this is a live system where even that gets reset every reboot.
+FIXME: Make machine-id consistent then we don't need to worry about any of this?
+
+ref: https://www.freedesktop.org/software/systemd/man/networkd.conf.html
+"""
+
+import argparse
+import pathlib
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('chroot_path', type=pathlib.Path)
+args = parser.parse_args()
+
+with open(args.chroot_path / pathlib.Path('etc/systemd/networkd.conf'), 'a') as f:
+    f.write('\n')  # Just in case the last line doesn't have a '\n'
+    f.write('\n'.join([
+        '[DHCPv4]',
+        'DUIDType=link-layer',
+        '[DHCPv6]',
+        'DUIDType=link-layer',
+    ]))
