423978fe9374eaa4bdf8cb84b91272d001e5d750
Mike Abrahall <mike@abrahall.id.au>
2023-02-23 11:40:20 +1100

    Added rsync-update-helper to jellyfin SOE

--- minecraft-server/rsync-update-helper.sh
+++ minecraft-server/rsync-update-helper.sh
@@ -1,11 +1,1 @@
-#!/bin/bash
-if ! echo "$@" | grep --quiet '/run/live/medium' ; then
-    rsync "$@"
-else
-    mount -o remount,rw /run/live/medium
-    mv /run/live/medium/previous /run/live/medium/$(date --rfc-3339=date)
-    mv /run/live/medium/latest /run/live/medium/previous
-    mkdir /run/live/medium/latest
-    rsync "$@"
-    mount -o remount,ro /run/live/medium
-fi
+../jellyfin-media-player/local-boot-updater/rsync-update-helper.sh
